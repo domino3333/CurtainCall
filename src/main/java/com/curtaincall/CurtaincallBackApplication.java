6afa249274a8bfab6d371334e80295e0d739edc1
domino3333 <busster@naver.com>
2026-05-19 23:50:02 +0900

    init

---
+++ src/main/java/com/curtaincall/CurtaincallBackApplication.java
@@ -0,0 +1,13 @@
+package com.curtaincall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurtaincallBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurtaincallBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/curtaincall/CurtaincallBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curtaincall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurtaincallBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
