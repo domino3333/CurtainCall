d47661d7048edb38fbbf040aa1b22d827b109469
domino3333 <busster@naver.com>
2026-05-25 04:34:09 +0900

    회원가입 api 구현 포스트맨 테스트 완료

--- src/main/java/com/curtaincall/controller/AuthController.java
+++ src/main/java/com/curtaincall/controller/AuthController.java
@@ -3,7 +3,9 @@ package com.curtaincall.controller;
 
 import com.curtaincall.dto.SignUpRequest;
 import com.curtaincall.mapper.MemberMapper;
+import com.curtaincall.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,16 +14,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 @RequestMapping("/api/member")
 public class AuthController {
 
+    private final MemberService memberService;
 
     @PostMapping
     public ResponseEntity<?> signUp(@RequestBody SignUpRequest request){
 
+        memberService.signUp(request);
+        log.info("signUp 컨트롤러 진입");
 
-
-
-        return null;
+        return ResponseEntity.ok("회원가입 성공");
     }
 }
