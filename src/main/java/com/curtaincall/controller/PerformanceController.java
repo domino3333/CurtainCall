fd89f5e63909fae2a7432ef036c80c8224f11aea
domino3333 <busster@naver.com>
2026-05-27 00:15:34 +0900

    주석 추가

--- src/main/java/com/curtaincall/controller/PerformanceController.java
+++ src/main/java/com/curtaincall/controller/PerformanceController.java
@@ -15,9 +15,12 @@ public class PerformanceController {
 
     private final PerformanceService performanceService;
 
+    /**
+    * 지역별 공연 정보 조회
+    * */
     @GetMapping("/area")
     public AreaPerformanceResponse getRegionalCultureInfo(){
-        
+
         //todo 데이터 구조 파악하기
         return performanceService.getRegionalCultureInfo();
 
