72c6cb10958394f92e53d5679706ba3020007f00
domino3333 <busster@naver.com>
2026-05-26 21:35:30 +0900

    restclientconfig 추가

--- src/main/java/com/curtaincall/client/PerformanceApiClient.java
+++ src/main/java/com/curtaincall/client/PerformanceApiClient.java
@@ -1,4 +1,13 @@
 package com.curtaincall.client;
 
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+@Component
+@RequiredArgsConstructor
 public class PerformanceApiClient {
+
+    private final RestClient restClient;
 }
