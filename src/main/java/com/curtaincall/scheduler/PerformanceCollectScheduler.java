99fa2d625a67381dab10a4b95f51274617fc151e
domino3333 <busster@naver.com>
2026-05-30 11:46:10 +0900

    스케쥴러와 컨트롤러 나누기 구상

--- src/main/java/com/curtaincall/scheduler/PerformanceCollectScheduler.java
+++ src/main/java/com/curtaincall/scheduler/PerformanceCollectScheduler.java
@@ -1,4 +1,18 @@
 package com.curtaincall.scheduler;
 
+
+
+
+/**
+ * api를 주기적으로 호출하여
+ * db에 저장하는 컨트롤러
+ * */
 public class PerformanceCollectScheduler {
+
+    //todo
+    // 그러면 performanceController는 어떻게 쓰여야맞는거지
+    // 지금은 외부 api호출을 performanceController에서 하고 있는데
+    // 그 호출을 그냥 여기서 하고, db저장도 여기서 하는 게 나을 듯
+    // performanceController에서는 사용자에게 정보 내려줄 때 사용
+
 }
