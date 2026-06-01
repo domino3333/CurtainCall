27614141485476bb43acf7c14db68aeb195244ad
domino3333 <busster@naver.com>
2026-06-02 01:14:45 +0900

    스케쥴러로 api호출 메서드 옮겨주고 1시간마다 호출하도록 수정

--- src/main/java/com/curtaincall/scheduler/PerformanceCollectScheduler.java
+++ src/main/java/com/curtaincall/scheduler/PerformanceCollectScheduler.java
@@ -1,12 +1,23 @@
 package com.curtaincall.scheduler;
 
 
-
+import com.curtaincall.dto.performance.area.AreaPerformanceResponse;
+import com.curtaincall.dto.performance.detail.PerformanceDetailResponse;
+import com.curtaincall.service.PerformanceService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 /**
  * api를 주기적으로 호출하여
  * db에 저장하는 컨트롤러
  * */
+@RequiredArgsConstructor
+@Slf4j
+@Component
 public class PerformanceCollectScheduler {
 
     //todo
@@ -14,5 +25,30 @@ public class PerformanceCollectScheduler {
     // 지금은 외부 api호출을 performanceController에서 하고 있는데
     // 그 호출을 그냥 여기서 하고, db저장도 여기서 하는 게 나을 듯
     // performanceController에서는 사용자에게 정보 내려줄 때 사용
+
+    private final PerformanceService performanceService;
+
+    /**
+     * 지역별 공연 정보 조회
+     * 1시간마다 api호출
+     * */
+    @GetMapping("/area")
+    @Scheduled(fixedRate = 3600000)
+    public AreaPerformanceResponse getRegionalCultureInfo(){
+
+        return performanceService.getRegionalCultureInfo();
+
+    }
+
+    /**
+     * 공연 정보 상세 조회
+     * 1시간마다 api호출
+     * */
+    @GetMapping("/detail/{seq}")
+    @Scheduled(fixedRate = 3600000)
+    public PerformanceDetailResponse getPerformanceDetail(@PathVariable Long seq){
+
+        return performanceService.getPerformanceDetail(seq);
+    }
 
 }
